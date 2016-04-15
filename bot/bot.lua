0818f11e26478e462350b7834c42c4c59de79f20
mobinlordshadow <mobinhackerrr@gmail.com>
2016-04-15 11:16:37 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {219339258},
+    sudo_users = {219339258,140768116},
     admin_users = {},
     disabled_channels = {}
   }
